9e3bc6782682e1da3bb05d93a16b3c925220cd0a
Blake Wright <btdubbzzz@gmail.com>
2025-01-25 23:19:05 -0600

    Add posters to results table

--- sixdegrees/src/components/ResultsTable.js
+++ sixdegrees/src/components/ResultsTable.js
@@ -26,7 +26,29 @@ const ResultsTable = ({ results }) => {
 				<TableBody>
 					{results.map((result, index) => (
 						<TableRow key={index}>
-							<TableCell>{result.path_sequence.join(' -> ')}</TableCell>
+							<TableCell>
+								{result.path_sequence.map((node, idx) => (
+									<span
+										key={idx}
+										style={{ display: 'flex', alignItems: 'center', gap: '10px' }}
+									>
+										{/* Check if node is a movie with a poster */}
+										{node.poster_url ? (
+											<img
+												src={`https://image.tmdb.org/t/p/w92${node.poster_url}`} // Example TMDb image base URL
+												alt={node.title || 'Poster'}
+												style={{
+													width: '50px',
+													height: '75px',
+													objectFit: 'cover',
+													borderRadius: '4px',
+												}}
+											/>
+										) : null}
+										<span>{node}</span>
+									</span>
+								))}
+							</TableCell>
 							<TableCell>{result.total_path_popularity}</TableCell>
 						</TableRow>
 					))}
